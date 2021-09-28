4512fe153c03835d9e996f2c1737b10f9bde59ee
james lockwood <ksjjl05@gmail.com>
2021-09-27 21:39:02 -0500

    adding the footer section into the site

--- ionictest/pages/index.js
+++ ionictest/pages/index.js
@@ -82,7 +82,8 @@ export default function Home(initialData) {
       </Row>
 
   </Row>
-  <Row> 
+  <Row>
+    <div className="cta"> 
   <Col>
     <div>
       <h4>Ready to start?</h4>
@@ -91,16 +92,58 @@ export default function Home(initialData) {
   </Col>
   <Col>
     <div>
-    <div className="mb-2">
-      <Button  className="heroBtn1" size="lg">
-        TRY IT FREE
-      </Button>
-      <Button className="heroBtn2" size="lg">
-        DOWNLOAD
-      </Button>
-  </div>
+      <div className="mb-2">
+        <Button  className="heroBtn1" size="lg">
+          TRY IT FREE
+        </Button>
+        <Button className="heroBtn2" size="lg">
+          DOWNLOAD
+        </Button>
+      </div>
     </div>
-  </Col>
+    
+    </Col>
+  </div>
+  </Row>
+  <Row>
+  <div className="d-flex flex-column h-100">
+
+<footer className="w-100 py-4 flex-shrink-0">
+    <div className="container py-4">
+        <div className="row gy-4 gx-5">
+            <div className="col-lg-4 col-md-6">
+                <h5 className="h1 text-white">FB.</h5>
+                <p className="small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                <p className="small text-muted mb-0">&copy; Copyrights. All rights reserved. <a className="text-primary" href="#">Bootstrapious.com</a></p>
+            </div>
+            <div className="col-lg-2 col-md-6">
+                <h5 className="text-white mb-3">Quick links</h5>
+                <ul className="list-unstyled text-muted">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Get started</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+            </div>
+            <div className="col-lg-2 col-md-6">
+                <h5 className="text-white mb-3">Quick links</h5>
+                <ul className="list-unstyled text-muted">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Get started</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+            </div>
+            <div className="col-lg-4 col-md-6">
+                <h5 className="text-white mb-3">Newsletter</h5>
+                <p className="small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+              
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+
   </Row>
 
   </Container>
